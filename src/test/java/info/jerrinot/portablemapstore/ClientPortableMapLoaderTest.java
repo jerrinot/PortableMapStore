81bf394d1f22762bd87984aae580a590cf31dac5
jerrinot <jaromir.hamala@gmail.com>
2021-03-06 17:48:43 +0100

    query test

--- src/test/java/info/jerrinot/portablemapstore/ClientPortableMapLoaderTest.java
+++ src/test/java/info/jerrinot/portablemapstore/ClientPortableMapLoaderTest.java
@@ -2,18 +2,25 @@ package info.jerrinot.portablemapstore;
 
 import com.hazelcast.client.HazelcastClient;
 import com.hazelcast.client.config.ClientConfig;
+import com.hazelcast.config.IndexConfig;
 import com.hazelcast.config.MapConfig;
+import com.hazelcast.config.MapStoreConfig;
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.map.IMap;
 import com.hazelcast.nio.serialization.GenericRecord;
+import com.hazelcast.query.Predicate;
+import com.hazelcast.query.Predicates;
 import info.jerrinot.portablemapstore.test.domain.Person;
 import info.jerrinot.portablemapstore.test.domain.PersonFactory;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.Collection;
+
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNull;
 
 public class ClientPortableMapLoaderTest extends BasePortableMapLoaderTest {
@@ -65,12 +72,34 @@ public class ClientPortableMapLoaderTest extends BasePortableMapLoaderTest {
         assertNull(map.get(ROW_COUNT + 1));
     }
 
+    @Test
+    public void testIndexedPredicateQuery() {
+        ClientConfig clientConfig = new ClientConfig();
+        clientConfig.getSerializationConfig().addPortableFactoryClass(1, PersonFactory.class.getName());
+        var client = HazelcastClient.newHazelcastClient(clientConfig);
+        configureMap(client);
+
+        IMap<Integer, Person> map = client.getMap("map");
+        Predicate<Integer, Person> predicate = Predicates.equal("lastname", "lastname1");
+        Collection<Person> values = map.values(predicate);
+        assertEquals(1, values.size());
+        Person person = values.iterator().next();
+        assertEquals(1, person.getId());
+        assertEquals("name1", person.getName());
+        assertEquals("lastname1", person.getLastname());
+        assertEquals(1, person.getDoubleField(), 0.1);
+        assertFalse(person.isBooleanField());
+    }
+
     private void configureMap(HazelcastInstance client) {
         MapConfig mapConfig = new MapConfig("map");
         mapConfig.getMapStoreConfig()
                 .setClassName(PortableMapLoader.class.getName())
                 .setProperties(createProps())
+                .setInitialLoadMode(MapStoreConfig.InitialLoadMode.EAGER)
                 .setEnabled(true);
+        mapConfig.addIndexConfig(new IndexConfig(IndexConfig.DEFAULT_TYPE, "lastname"));
+
         client.getConfig().addMapConfig(mapConfig);
     }
 }
